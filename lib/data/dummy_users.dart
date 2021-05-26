9a87e64a842d45bde764b2ead610ac4e69a01f80
Sidney <sidneyjunior26@gmail.com>
2021-05-26 20:02:14 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Sidney',
+      email: 'sidneyjunior26@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/04/24/18/21/naruto-6204734_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Ingrid',
+      email: 'Ingrid@Ingrid.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407_960_720.png'),
+  '3': const User(
+      id: '3', name: 'Sirlene',
+      email: 'Sirlene@Sirlene.com.br',
+      avatarUrl: ''),
+};
